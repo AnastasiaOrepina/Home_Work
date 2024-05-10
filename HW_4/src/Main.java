7bd5327f0ffcb5ddd9820fa904076669a0864745
AnastasiaOrepina <168459496+AnastasiaOrepina@users.noreply.github.com>
2024-05-10 22:54:03 +0300

    git commit -m "1, 2" git commit -m "1, 2" git commit -m "1, 5"

--- HW_4/src/Main.java
+++ HW_4/src/Main.java
@@ -1,21 +1,59 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
+        printColor();
+        compareNumbers();
+        checkSumPositive();
     }
+
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
+
     public static void checkSumSign() {
-        int a = 90;
+        int a = 100;
         int b = 100;
         int sumAb = a + b;
         if (sumAb >= 0) {
             System.out.println("Сумма положительная");
         } else {
             System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 111;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+    public static void compareNumbers() {
+        int a = 100;
+        int b = 100;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+    public static void checkSumPositive() {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int sumAb = a + b;
+        if (sumAb >= 10 && sumAb <= 20) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
         }
     }
 }
